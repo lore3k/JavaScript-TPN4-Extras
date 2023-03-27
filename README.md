f846360656c1ad1d4f2c29f331defe296fb0702d
Lore Ginard <lorenaginard@hotmail.com>
2023-03-27 12:36:45 -0300

    Enunciados

---
+++ README.md
@@ -0,0 +1,10 @@
+## Ejercicios Extras del Trabajo Práctico N°4
+
+Conceptos POO (Programación Orientada a Objetos).
+
+- Clases
+- Objetos
+- Métodos
+
+## Autor
+Lorena Ginard
---
+++ js/piedraPapelTijera.js
@@ -0,0 +1,4 @@
+/*Piedra-papel-tijera 👊🏻 ✋🏻 ✌🏻 :crear un script (en el repo de conceptos de JS o 
+realizar un nuevo repo) con toda la logica necesaria que solicite al usuario 
+un movimiento (piedra - papel - tijera) la PC elija aleatoriamente un movimiento 
+y al final mostrar por pantalla quien gano, luego preguntar si queremos jugar otra vez. */
